7c4a41464933d496ba9636982db7f5479753a679
akahard13 <harnaldosanchez49@gmail.com>
2025-01-30 14:24:53 -0600

    MEJORAS

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('verBtn').addEventListener('click', function() {
+    window.location.href = 'flowers.html'; // Cambia 'flower.html' por el nombre de tu página principal
+});
